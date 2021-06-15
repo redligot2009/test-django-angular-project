d80b94298133b9009395118dcad681e01746ebb5
Ian Red Ligot <redligot2009.forreal@gmail.com>
2021-06-15 08:44:57 +0800

    Move components to components folder (Angular frontend)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { TodoListsPageComponent } from './todo-lists-page/todo-lists-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { TodoListsPageComponent } from './components/todo-lists-page/todo-lists-page.component';
 
 const routes: Routes = [
   {path:'register', component: RegisterPageComponent},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { TodoListsPageComponent } from './todo-lists-page/todo-lists-page.component';
-import { TodoListDetailsPageComponent } from './todo-list-details-page/todo-list-details-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { TodoListsPageComponent } from './components/todo-lists-page/todo-lists-page.component';
+import { TodoListDetailsPageComponent } from './components/todo-list-details-page/todo-list-details-page.component';
 
 import {MatGridListModule} from '@angular/material/grid-list';
 
@@ -24,6 +25,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatGridListModule
